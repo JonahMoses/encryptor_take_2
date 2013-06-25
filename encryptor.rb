79e3af23726a805f37a33635c5529aba9842bdd8
Jonah <jonahkmoses@gmail.com>
2013-06-24 17:39:36 -0700

    Refactored .each to .collect for the encrypt method

--- encryptor.rb
+++ encryptor.rb
@@ -15,17 +15,29 @@ class Encryptor
     	cipher[lowercase_letter]
     end
 
+    # def encrypt_letter(letter)
+    # 	lowercase_letter = letter.downcase
+    # 	cipher[lowercase_letter]
+    # end
+
     def encrypt(string)
-    	# 1. Cut the input string into letters
     	letters = string.split("")
-  		# 2. Encrypt those letters one at a time, gathering the results
-  		results = []
-  		letters.each do |letter|
-  			encrypted_letter = encrypt_letter(letter)
-  			results.push(encrypted_letter)
-  		end
-  		# 3. Join the results back together in one string
-  		results.join 
+
+    	encrypted_letter = letters.collect do |letter|
+  			encrypt_letter(letter)
+  		end.join
+  		
     end
+
+    # def encrypt(string)
+	# 	letters = string.split("")
+    	
+	#   results = []
+	#   letters.each do |letter|
+	#   	encrypted_letter = encrypt_letter(letter)
+	#   	results.push(encrypted_letter)
+	#   end
+	#   results.join
+	# end
 
 end
