6bc570cf0a49456051f45119accc7982605720fa
Jonah <jonahkmoses@gmail.com>
2013-06-24 18:05:30 -0700

    Added the ability to change rotational value

--- encryptor.rb
+++ encryptor.rb
@@ -6,18 +6,7 @@ class Encryptor
 		Hash[characters.zip(rotated_characters)]
     end
 
-  #   def cipher
-		# {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
-  #    	'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
-  #    	'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
-  #    	'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
-  #    	'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
-  #    	'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
-  #    	'y' => 'l', 'z' => 'm'}
-  #   end
-
-    def encrypt_letter(letter)
-    	rotation = 13
+    def encrypt_letter(letter,rotation)
     	cipher_for_rotation = cipher(rotation)
     	cipher_for_rotation[letter]
     end
