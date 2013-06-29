bfe06e00e07f3bc66e1a3138ed51ec9dd8b89db8
Jonah <jonahkmoses@gmail.com>
2013-06-28 17:42:41 -0700

    added supported_characters and crack methods

--- encryptor.rb
+++ encryptor.rb
@@ -67,6 +67,15 @@ class Encryptor
     decrypted_file.close
     end
 
+    def supported_characters
+        (' '..'z').to_a
+    end
+
+    def crack(message)
+        supported_characters.count.times.collect do |attempt|
+            decrypt(message,attempt)
+        end
+    end
 
 
 
